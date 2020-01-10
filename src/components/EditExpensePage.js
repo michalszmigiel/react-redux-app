7b679c47f18a4a5f0a97492e8842d775dedecdc7
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2020-01-10 09:33:38 +0100

    added date picker filter

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import { connect } from "react-redux";
-import { editExpense } from "../actions/expenses";
+import { editExpense, removeExpense } from "../actions/expenses";
 
 const EditExpensePage = props => (
   <div>
@@ -11,9 +11,16 @@ const EditExpensePage = props => (
       expense={props.expense}
       onSubmit={expense => {
         props.dispatch(editExpense(props.match.params.id, expense));
-        props.history.push("/");
       }}
     />
+    <button
+      onClick={() => {
+        props.dispatch(removeExpense({ id: props.expense.id }));
+        props.history.push("/");
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { removeExpense } from "../actions/expenses";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
-      <h3>description: {description}</h3>
+      <h3>{description}</h3>
     </Link>
     <p>amount:{amount}</p>
     <p>created at: {createdAt}</p>
-    <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
